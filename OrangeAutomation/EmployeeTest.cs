70f00bb9bf9b8a528cfbe02c3b816328ba33024d
Administrator <Administrator@WINDOWS-UNR7VLH>
2022-11-22 12:26:41 +0530

    added [TestCaseSource] in same class

--- OrangeAutomation/EmployeeTest.cs
+++ OrangeAutomation/EmployeeTest.cs
@@ -10,19 +10,46 @@ namespace Fujitsu.OrangeAutomation
 {
     public class EmployeeTest : AutomationWrapper
     {
-        [Test]
-        public void AddValidEmployeeTest()
+
+        public static object[] AddValidEmployeeData()
         {
-            driver.FindElement(By.Name("username")).SendKeys("admin");
-            driver.FindElement(By.CssSelector("[name='password']")).SendKeys("admin123");
+            string[] dataSet1 = new string[6];
+            dataSet1[0] = "admin";
+            dataSet1[1] = "admin123";
+            dataSet1[2] = "John";
+            dataSet1[3] = "L";
+            dataSet1[4] = "Wick";
+            dataSet1[5] = "John Wick";
+
+            string[] dataSet2 = new string[6];
+            dataSet2[0] = "admin";
+            dataSet2[1] = "admin123";
+            dataSet2[2] = "Paul";
+            dataSet2[3] = "k";
+            dataSet2[4] = "Peter";
+            dataSet2[5] = "Paul Peter";
+
+            object[] allDataSet = new object[2];
+            allDataSet[0] = dataSet1;
+            allDataSet[1] = dataSet2;
+
+            return allDataSet;
+        }
+
+        [Test,TestCaseSource(typeof(EmployeeTest),nameof(AddValidEmployeeData))]
+        public void AddValidEmployeeTest(string username,string password,string firstname,string middlename,string lastname
+            ,string expectedName)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(username);
+            driver.FindElement(By.CssSelector("[name='password']")).SendKeys(password);
             driver.FindElement(By.XPath("//button[normalize-space()='Login']")).Click();
             
             driver.FindElement(By.XPath("//span[text()='PIM']")).Click();
             driver.FindElement(By.LinkText("Add Employee")).Click();
 
-            driver.FindElement(By.Name("firstName")).SendKeys("John");
-            driver.FindElement(By.Name("middleName")).SendKeys("W");
-            driver.FindElement(By.Name("lastName")).SendKeys("Wick");
+            driver.FindElement(By.Name("firstName")).SendKeys(firstname);
+            driver.FindElement(By.Name("middleName")).SendKeys(middlename);
+            driver.FindElement(By.Name("lastName")).SendKeys(lastname);
 
             driver.FindElement(By.XPath("//button[normalize-space()='Save']")).Click();
 
@@ -30,7 +57,7 @@ namespace Fujitsu.OrangeAutomation
             Thread.Sleep(8000);
 
             string actualAddedName = driver.FindElement(By.XPath("//div[@class='orangehrm-edit-employee-name']//h6")).Text;
-            Assert.That(actualAddedName, Is.EqualTo("John Wick"));
+            Assert.That(actualAddedName, Is.EqualTo(expectedName));
         }
     }
 }
